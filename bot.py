92370e1053133a3958de15c689159289bbb7ec49
MattMoony <m4ttm00ny@gmail.com>
2020-11-24 15:09:04 +0100

    :sparkles: tried to improve memory usage + new templates

--- bot.py
+++ bot.py
@@ -18,6 +18,9 @@ class MissingFontError(Exception):
 class MissingMemeJPGError(Exception):
     pass
 
+class TooFewArgumentsError(Exception):
+    pass
+
 class MemeTemplate(object):
     def __init__(self, im_path: str, config: object, font_path: str):
         self.im_path: str = im_path
@@ -30,7 +33,10 @@ class MemeTemplate(object):
         io = BytesIO()
         dr.multiline_text(tuple(self.config['from']), '\n'.join(textwrap.wrap(txt, width=self.config['width'])), fill=tuple(self.config['colour']), font=ImageFont.truetype(self.font_path, size=self.config['font_size']))
         im.save(io, format='PNG')
-        return BytesIO(io.getvalue())
+        im.close()
+        ret = BytesIO(io.getvalue())
+        io.close()
+        return ret
 
 class Gagmachine(dc.Client):
     def __init__(self, tkn: str, meme_path: str = os.path.join(BPATH, 'memes'), asset_path: str = os.path.join(BPATH, 'assets')):
@@ -41,6 +47,7 @@ class Gagmachine(dc.Client):
         self.__cmds: Dict[str, Callable[Any, None]] = {
             'ping': self.pong,
             'refresh': self.refresh,
+            '*': self.list_all,
         }
         self.__memes: Dict[str, MemeTemplate] = {}
         if not os.path.isfile(os.path.join(self.asset_path, 'font.ttf')):
@@ -89,15 +96,21 @@ class Gagmachine(dc.Client):
         self.scan_memes()
         await msg.channel.send(f'Scanned for new meme templates ... Found {len(self.__memes)} ... ')
 
+    async def list_all(self, msg: dc.Message) -> None:
+        await msg.channel.send('Alwight geezer, here be all ma templates:')
+        await msg.channel.send()
+
     async def send_meme(self, msg: dc.Message, meme: MemeTemplate) -> None:
         if len(msg.content.split(' ')) < 3:
             await msg.channel.send('Ya do need to provide some text, ya geezer!')
             return
-        await msg.channel.send(file=dc.File(meme.make(' '.join(msg.content.split(' ')[2:])), 'meme.jpg'))
+        f = meme.make(' '.join(msg.content.split(' ')[2:]))
+        await msg.channel.send(file=dc.File(f, 'meme.png'))
+        f.close()
 
 def main():
     if not os.environ['GAG_TKN']:
-        print('\033[33m[-] Missing "GAG_TKN" in environment variables ... Exiting! ')
+        print('\033[33m[-] Missing "GAG_TKN" in environment variables ... Exiting! \033[0m')
         os._exit(1)
     TKN: str = os.environ['GAG_TKN']
 
